dc6140fd89102610dd38b52a71acba8886d41983
nahrinoda <nahrinoda@yahoo.com>
2019-07-02 15:05:21 -0400

    changed greeting

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send({ hi: "there" });
+  res.send("<h1>Hi There</h1>");
 });
 
 const PORT = process.env.PORT || 5000;
