2e2e36677166b957aab3b83baa81801f6a9a845f
nahrinoda <nahrinoda@yahoo.com>
2019-07-19 14:20:50 -0400

    removed package-lock.json

---
+++ server/client/src/components/surveys/SurveyNew.js
@@ -0,0 +1,15 @@
+// Surveynew show SurveyForm and SurveyForm Review
+import React, { Component } from "react";
+import SurveyForm from "./SurveyForm";
+
+class SurveyNew extends Component {
+  render() {
+    return (
+      <div>
+        <SurveyForm />
+      </div>
+    );
+  }
+}
+
+export default SurveyNew;
